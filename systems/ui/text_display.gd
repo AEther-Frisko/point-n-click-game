19921edfabeb9778bbb2902137a281d746d4e8ad
max <ajvwolf@gmail.com>
2026-04-02 00:38:52 -0400

    gui ignores mouse input, small visual changes

--- systems/ui/text_display.gd
+++ systems/ui/text_display.gd
@@ -2,7 +2,7 @@ extends Label
 
 # Definitely will not be staying this way, just testing
 func _ready() -> void:
-	var object = get_node("../../World/Object")
+	var object = get_node("../../../World/Object")
 	object.object_clicked.connect(_on_object_clicked)
 	object.object_hovered.connect(_on_object_hovered)
 
