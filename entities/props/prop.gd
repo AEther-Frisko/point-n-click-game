6a13aca54c8b5914dff663867687f89e7033a42e
max <ajvwolf@gmail.com>
2026-04-05 23:36:23 -0400

    created prop data resource for defining props

--- entities/props/prop.gd
+++ entities/props/prop.gd
@@ -1,12 +1,22 @@
 extends Node2D
 
-@export var description: String = "This is an object."
+@export var propData: PropData
 
 signal prop_clicked(desc: String)
 
 func _ready() -> void:
+	# set prop sprite
+	var sprite := get_node("Sprite2D")
+	sprite.texture = propData.texture
+	
+	# set collision shape to match sprite size
+	var texture_size = sprite.texture.get_size()
+	texture_size = texture_size * sprite.scale
+	var collision_shape := get_node("ClickableArea/CollisionShape2D")
+	collision_shape.shape.size = texture_size
+	
 	var clickable_area = get_node("ClickableArea")
 	clickable_area.clicked.connect(_on_clicked)
-	
+
 func _on_clicked() -> void:
-	prop_clicked.emit(description)
+	prop_clicked.emit(propData.description)
---
+++ entities/props/prop_data.gd
@@ -0,0 +1,5 @@
+class_name PropData
+extends Resource
+
+@export var description: String = "Object description"
+@export var texture: Texture2D
