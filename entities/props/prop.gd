9b477b748e3247c071e98c277880267a98f3fdff
max <ajvwolf@gmail.com>
2026-04-06 00:17:13 -0400

    cursor changes when hovering over props (using ugly temp texture)

--- entities/props/prop.gd
+++ entities/props/prop.gd
@@ -1,13 +1,14 @@
 extends Node2D
 
-@export var propData: PropData
+@export var prop_data: PropData
+@export var cursor: Texture2D
 
 signal prop_clicked(desc: String)
 
 func _ready() -> void:
 	# set prop sprite
 	var sprite := get_node("Sprite2D")
-	sprite.texture = propData.texture
+	sprite.texture = prop_data.texture
 	
 	# set collision shape to match sprite size
 	var texture_size = sprite.texture.get_size()
@@ -15,8 +16,18 @@ func _ready() -> void:
 	var collision_shape := get_node("ClickableArea/CollisionShape2D")
 	collision_shape.shape.size = texture_size
 	
+	# connect clickable area signals
 	var clickable_area = get_node("ClickableArea")
 	clickable_area.clicked.connect(_on_clicked)
+	clickable_area.mouse_entered.connect(_on_hover_start)
+	clickable_area.mouse_exited.connect(_on_hover_end)
 
 func _on_clicked() -> void:
-	prop_clicked.emit(propData.description)
+	prop_clicked.emit(prop_data.description)
+
+func _on_hover_start() -> void:
+	Input.set_custom_mouse_cursor(cursor)
+
+func _on_hover_end() -> void:
+	pass
+	Input.set_custom_mouse_cursor(null)
